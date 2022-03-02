1013d14bf1af5523182c55657ce633c3dc842b1f
Michael Walker <dev@michaelwalker.us>
2022-03-01 19:43:55 -0500

    Added arrow function to java debug file. Updated variable and keyword mapping.

--- debug/java/DuskyTheme.java
+++ debug/java/DuskyTheme.java
@@ -1,5 +1,13 @@
 package debug.java;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DuskyTheme implements ITheme {
 
+  public void arrowFunction(String param) {
+    List<String> list = new ArrayList<>();
+    list.add(param);
+    list.stream().filter(predicate -> predicate.length() > 5);
+  }
 }
