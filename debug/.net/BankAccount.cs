6b5688075df6eaea045e48385dff9a8b36d2de9b
Michael Walker <dev@michaelwalker.us>
2023-12-03 18:06:09 -0500

    Added .net debug class.

---
+++ debug/.net/BankAccount.cs
@@ -0,0 +1,30 @@
+namespace Classes;
+
+public class BankAccount
+{
+    private List<Transaction> _allTransactions = new List<Transaction>();
+    public string Number { get; }
+    public string Owner { get; set; }
+    public decimal Balance { get; }
+
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+        Console.WriteLine($"Account {this.Number} was created for {this.Owner} with {this.Balance} initial balance.");
+
+    }
+
+    [AuthenticationRequired]
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+        }
+        if (Balance - amount < 0)
+        {
+            throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+        }
+        var withdrawal = new Transaction(-amount, date, note);
+        _allTransactions.Add(withdrawal);
+    }
+}
